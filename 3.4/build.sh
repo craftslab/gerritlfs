0f04c5ea1366d08e1fbad8f2e43e9ad1f136629b
Jia Jia <angersax@sina.com>
2026-01-16 10:22:23 +0800

    fix(build): fix building error caused by script file

--- 3.4/build.sh
+++ 3.4/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build gerrit-plugins-lfs:3.4 .
+docker build -t gerrit-plugins-lfs:3.4 .
 docker run --name gerrit-plugins-lfs gerrit-plugins-lfs:3.4
 docker cp gerrit-plugins-lfs:/workspace/output/lfs.jar .
 docker rm gerrit-plugins-lfs
