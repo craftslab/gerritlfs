02a8ddb128729de1389d02327f697b6751befa93
Jia Jia <angersax@sina.com>
2026-01-16 18:38:03 +0800

    refactor(build): update script for building lfs plugin on gerrit 2.13 & 3.4

--- 2.13/build.sh
+++ 2.13/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker build -t gerrit-plugins-lfs:2.13 .
-docker run --name gerrit-plugins-lfs gerrit-plugins-lfs:2.13
+docker run -it -d --name gerrit-plugins-lfs gerrit-plugins-lfs:2.13
 docker cp gerrit-plugins-lfs:/workspace/lfs-2.13/buck-out/gen/lfs.jar .
-docker rm gerrit-plugins-lfs
+docker rm -f gerrit-plugins-lfs
--- 3.4/build.sh
+++ 3.4/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker build -t gerrit-plugins-lfs:3.4 .
-docker run --name gerrit-plugins-lfs gerrit-plugins-lfs:3.4
+docker run -it -d--name gerrit-plugins-lfs gerrit-plugins-lfs:3.4
 docker cp gerrit-plugins-lfs:/workspace/output/lfs.jar .
-docker rm gerrit-plugins-lfs
+docker rm -f gerrit-plugins-lfs
